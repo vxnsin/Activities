98333c4362a7163cf833f08bfc277211e5e3b5f7
Skully <117316808+skullysmods@users.noreply.github.com>
2026-03-25 09:51:18 +0100

    feat(DARK.OUT): add activity (#10583)

---
+++ websites/D/DARK.OUT/presence.ts
@@ -0,0 +1,44 @@
+const presence = new Presence({
+  clientId: '1478388131903570197',
+})
+
+enum ActivityAssets {
+  Logo = 'https://g.tobyjas.dev/darkout/icon-512.png',
+}
+
+interface StatusData {
+  details: string
+  state: string
+  largeImage: string
+  startTimestamp: number
+}
+
+presence.on('UpdateData', async () => {
+  const statusData = await presence.getPageVariable<StatusData>(
+    'details',
+    'state',
+    'largeImage',
+    'startTimestamp',
+  )
+
+  const presenceData: PresenceData = {
+    details: 'Browsing...',
+    largeImageKey: ActivityAssets.Logo,
+  }
+
+  if (statusData.details)
+    presenceData.details = statusData.details
+  if (statusData.state)
+    presenceData.state = statusData.state
+  if (statusData.startTimestamp)
+    presenceData.startTimestamp = statusData.startTimestamp
+  if (statusData.largeImage)
+    presenceData.largeImageKey = statusData.largeImage
+
+  if (presenceData.details) {
+    presence.setActivity(presenceData)
+  }
+  else {
+    presence.clearActivity()
+  }
+})
